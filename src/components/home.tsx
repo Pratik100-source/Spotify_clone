f667a290c93a6dfa92285ec7c31e2b80b106e0a8
Pratik100-source <panthipratik100@gmail.com>
2024-07-19 21:32:33 +0545

    some changes has been made

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,13 +1,31 @@
 import React from "react";
-import {View, Text} from 'react-native';
+import {View, Text, Alert, Button, TouchableOpacity} from 'react-native';
 import { StyleSheet } from "react-native";
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faBell } from "@fortawesome/free-regular-svg-icons/faBell";
+import { faClock } from "@fortawesome/free-regular-svg-icons/faClock";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+
+
+
+
 
 const Home = () =>{
 
+
     return(
        <View style={styles.container}>
+       <View style={styles.header}>
+        <Text style={styles.header_text}>Good morning</Text>
 
-        <Text style={{color:"#fff"}}>Welcome to Spotify</Text>
+        <View style={styles.header_icon}>
+       <FontAwesomeIcon icon={faBell} size={30} color="white"></FontAwesomeIcon>
+       <FontAwesomeIcon icon={faClock} size={30} color="white"></FontAwesomeIcon>
+       <FontAwesomeIcon icon={faGear} size={30} color="white"></FontAwesomeIcon>
+       </View>
+       
+       </View>
+       
 
        </View>
     )
@@ -18,13 +36,39 @@ export const styles= StyleSheet.create({
 
     container:{
         flex:1,
-        alignItems:"center",
-        justifyContent:"center",
         position:"absolute",
         backgroundColor:"#000000",
         width:"100%",
-        height:"100%"
-    }
+        height:"100%",
+
+    },
+    header:{
+        position:"absolute",
+        width:"100%",
+        height:"30%",
+        paddingTop: 83,
+        paddingRight:15,
+        paddingLeft: 15,
+        paddingBottom:0,
+        display:"flex",
+        flexDirection:"row",
+        // backgroundColor:"white"
+    },
+    header_text:{
+       color:"#fff",
+       fontSize:30,
+       flex:0.7,
+    //    paddingLeft:15
+    },
+    header_icon:{
+        flex:0.3,
+        color:"white",
+        marginRight:"3%",
+        display:"flex",
+        flexDirection:"row",
+        gap:15,
+        paddingTop:9
+    },
 })
 
 export default Home;
