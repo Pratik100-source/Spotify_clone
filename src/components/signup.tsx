f6ce29011fdcac075af8bc5c09f5ec4f7e278d04
Pratik100-source <panthipratik100@gmail.com>
2024-07-22 13:54:06 +0545

    Signup page frontend complete

--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -18,14 +18,14 @@ const Signup = () =>{
             <View style={styles.logo_div}>
             <Image source={Logo} style={styles.logo}></Image>
             <Text style={styles.text}>Millions of Songs,</Text>
-            <Text style={[styles.text,{marginTop:-35}]}>Free on Spotify</Text>
+            <Text style={[styles.text,{marginTop:-40}]}>Free on Spotify</Text>
             </View>
             <View style={styles.credential_div}>
                 {/* <TextInput placeholder="Type something" style={styles.text_Input}></TextInput> */}
                 <TouchableOpacity style={styles.credential}><Text style={styles.credential_text}>Sign up free</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.credential_aarko}><FontAwesomeIcon icon={faGoogle} style={[styles.credential_icon, {right:50}]} size={25}></FontAwesomeIcon><Text style={styles.credential_text}>Continue with Google</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.credential_aarko}><FontAwesomeIcon icon={faFacebook} style={[styles.credential_icon, {right:40}]} size={25}></FontAwesomeIcon><Text style={styles.credential_text}>Continue with Facebook</Text></TouchableOpacity>
-                <Text style={{fontSize:14,}}>Login</Text>
+                <TouchableOpacity style={styles.credential_aarko}><FontAwesomeIcon icon={faGoogle} style={[styles.credential_icon, {right:50}]} size={25} color="white"></FontAwesomeIcon><Text style={styles.credential_text}>Continue with Google</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.credential_aarko}><FontAwesomeIcon icon={faFacebook} style={[styles.credential_icon, {right:40}]} size={25} color="white"></FontAwesomeIcon><Text style={styles.credential_text}>Continue with Facebook</Text></TouchableOpacity>
+                <Text style={{fontSize:20, textAlign:"center",marginTop:20,color:"white"}}>Login</Text>
             </View>
         </View>
     </View>
@@ -36,36 +36,39 @@ export const styles = StyleSheet.create({
 
     main_div:{
             flex:1,
-            backgroundColor:"black",
+            backgroundColor:"#191414",
             justifyContent:"center",
             paddingRight:15,
             paddingLeft:15,
             alignItems:"center",
             width:"100%",
+
             
     },
 
     submain_div:{
             display:"flex",
             flexDirection:"column",
-            backgroundColor:"white",
+            // backgroundColor:"white",
             height:"65%",
-            marginTop:310
+            marginTop:300,
+            paddingTop:60
 
     },
 
     logo_div:{
-          flex:0.4,
+          flex:0.45,
           display:"flex",
-          flexDirection:"column"
+          flexDirection:"column",
+        //   paddingTop:50
     },
     logo:{
-        width: "30%",
-        height:"30%",
+        width:80,
+        height:80,
         position:"absolute",
         objectFit:"contain",
-        marginLeft:"35%",
-        marginRight:"50%",
+        marginLeft:"40%",
+        marginRight:"40%",
         marginTop:0,
         marginBottom:0,
         flex: 0.4,
@@ -77,14 +80,16 @@ export const styles = StyleSheet.create({
         fontSize:35,
         flex:0.3,
         textAlign:"center",
-        top:90,
+        top:100,
+        color:"white"
+        
 
         
     },
 
     credential_div:{
-        flex:0.6,
-        backgroundColor:"yellow",
+        flex:0.5,
+        // backgroundColor:"yellow",
         width:380,
         paddingLeft:20,
         paddingRight:20
@@ -93,14 +98,14 @@ export const styles = StyleSheet.create({
    
     credential:{  
 
-       borderWidth:1,
-       borderColor:"green",
+       borderWidth:0.4,
+       borderColor:"white",
        borderRadius:10,
        height:50,
        width:340,
        alignItems:"center",
        justifyContent:"center",
-       backgroundColor:"white",
+    //    backgroundColor:"white",
        borderTopLeftRadius:18,
        borderTopRightRadius:18,
        borderBottomLeftRadius:18,
@@ -111,14 +116,14 @@ export const styles = StyleSheet.create({
     },
 
     credential_aarko:{
-        borderWidth:1,
-        borderColor:"black",
+        borderWidth:0.4,
+        borderColor:"white",
         borderRadius:10,
         height:50,
         width:340,
         alignItems:"center",
         justifyContent:"center",
-        backgroundColor:"white",
+        // backgroundColor:"white",
         display:"flex",
         flexDirection:"row",
         marginTop:8,
@@ -132,11 +137,13 @@ export const styles = StyleSheet.create({
     credential_text:{
 
         fontSize:20,
+        color:"white"
     },
 
     credential_icon:{
           
         // right:50,
+        // color:"white"
     }
 
 })
